1095df10f0e06895dac18971b7ae65f95b406c92
cuonglp <cuongfpoly813@gmail.com>
2024-06-11 22:12:20 +0700

    Describe your changes here

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                base: resolve(__dirname, 'src/pages/base.html'),
-                Order: resolve(__dirname, 'src/pages/Order.html'),
-            },
-        },
-    },
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				category: resolve(__dirname, "src/pages/category.html"),
+				base: resolve(__dirname, "src/pages/base.html"),
+				base: resolve(__dirname, "src/pages/products.html"),
+			},
+		},
+	},
 });
-
