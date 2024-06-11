c85c55a917f6df902850a7ab09f4fe03224144b7
cuonglp <cuongfpoly813@gmail.com>
2024-06-11 22:02:15 +0700

    Describe your changes here

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 base: resolve(__dirname, 'src/pages/base.html'),
+                Order: resolve(__dirname, 'src/pages/Order.html'),
             },
         },
     },
